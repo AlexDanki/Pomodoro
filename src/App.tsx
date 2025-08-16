268a400ff878ec366cb605a8e686e3fab891100d
Alexsandro Duarte <pocmusic321@gmail.com>
2025-08-16 13:28:20 -0300

    esqueleto básico da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+    <div className="container">
+
+      <div className='card'>
+
+        <section className="buttons-timer">
+          <button className='btn-time'>Pomodoro</button>
+          <button className='btn-time'>Short Break</button>
+          <button className='btn-time'>Long Break</button>
+        </section>
+
+        <h1>00:00</h1>
+
+        <button className="btn-pause">Start</button>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
+
   )
 }
 
